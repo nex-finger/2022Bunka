79cb92254d5615a7e1d7cf66c128c8ac08bbac86
OrangeP1anet <kass12092@outlook.jp>
2022-03-14 02:37:50 +0900

    Add ExitGameScript
    VoyageやLandingのシーンではEscキーを押した際にTitleシーンに移動、TitleシーンでEscを押した際にはGameTitleに移動するスクリプトを追加(ExitGame.cs)。GameTitleのExitボタンを押した際にゲーム終了。各スクリプトはMainCameraに配置。

---
+++ Assets/Script/GameTitle/SC_ExitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_ExitGame : MonoBehaviour
+{
+    public void OnClickExitButton() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) {
+            SceneManager.LoadScene("GameTitle");
+        }
+    }
+}
