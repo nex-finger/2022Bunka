fd22ccb4ced93af92ce9b219e6d57ba4d951df23
nex-finger <2020Tibakou@gmail.com>
2022-03-14 20:47:22 +0900

    はい
    はい

---
+++ Assets/Script/RocketComponent/LevelStorage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    public Text label;
+    public int count;
+
+    void Start()
+    {
+        count = 2;
+        UpdateLabel();
+    }
+
+    // 数値を増やすボタンのOn Click()に指定
+    public void Click1()
+    {
+        // UpdateLabel()内で分岐をすると、表示とcountの値がずれてしまうので、移行して条件も少し変更
+        if (count < 30)
+        {
+            count++;
+        }
+        UpdateLabel();
+    }
+
+    // 数値を減らすボタンのOn Click()に指定
+    public void Click2()
+    {
+        if (count > 2)
+        {
+            count--;
+        }
+        UpdateLabel();
+    }
+
+    void UpdateLabel()
+    {
+        label.text = count + "人";
+    }
+}
--- Assets/Script/StopScript.cs
+++ Assets/Script/StopScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StopScript : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("hit");
         this.GetComponent<Lander>().enabled = false;
