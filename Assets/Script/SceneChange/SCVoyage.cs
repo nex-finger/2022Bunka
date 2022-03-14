ed27468eef756e3b259d1c5f3288d1d8b533333d
nex-finger <2020Tibakou@gmail.com>
2022-03-14 17:10:58 +0900

    HowtoPlayシーンつくりました
    はい

---
+++ Assets/Script/SceneChange/SCVoyage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCVoyage : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Voyage");
+    }
+}
