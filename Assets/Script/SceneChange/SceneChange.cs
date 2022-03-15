94b152bab385e053d7a2711beb956794a3a3aa42
nex-finger <2020Tibakou@gmail.com>
2022-03-15 16:27:34 +0900

    hai
    hai

--- Assets/Script/SceneChange/SceneChange.cs
+++ Assets/Script/SceneChange/SceneChange.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SCHowtoPlay()
     {
-        
+        SceneManager.LoadScene("HowtoPlay");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
