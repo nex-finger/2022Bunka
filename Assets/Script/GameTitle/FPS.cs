57715433fdae52755c3544d041b9c8525fb1316e
nex-finger <2020Tibakou@gmail.com>
2022-03-18 19:15:22 +0900

    debug
    はい

--- Assets/Script/GameTitle/FPS.cs
+++ Assets/Script/GameTitle/FPS.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPS : MonoBehaviour
 {
+    public Text Textb;
+    private float Timeb;
+    private int FPSb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Timeb = 0.0f;
+        FPSb = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timeb += Time.deltaTime;
+        FPSb += 1;
+
+        Textb.text = string.Format("{0}", FPSb);
+
+        if (Timeb > 1.0f)
+        {
+            Timeb -= 1.0f;
+            FPSb = 0;
+        }
     }
 }
---
+++ Assets/Script/Test/TestTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTime : MonoBehaviour
+{
+    public Text Texta;
+    private float Timea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timea = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timea += Time.deltaTime;
+        Texta.text = string.Format("{0}", Timea);
+    }
+}
