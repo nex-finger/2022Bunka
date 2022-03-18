dd4010854cd23b37b70116af09e6de057a685fbf
nex-finger <2020Tibakou@gmail.com>
2022-03-19 03:04:15 +0900

    hai
    hai

--- Assets/Script/Result 1/ResultStorage.cs
+++ Assets/Script/Result 1/ResultStorage.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class ResultStorage : MonoBehaviour
 {
+    private int Score;
+    private float Time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Score = 3000 - LevelStorage.GetCash();
+        Time = LevelStorage.GetTotalTime(0);
     }
 }
