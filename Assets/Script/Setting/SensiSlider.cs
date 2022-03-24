d040ec0340bd25b17ad4d589fb738a075a22f748
nex-finger <2020Tibakou@gmail.com>
2022-03-25 02:48:42 +0900

    はい
    はい

---
+++ Assets/Script/Setting/SensiSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//Sliderを使用するために必要
+
+[RequireComponent(typeof(Slider))]
+
+public class SensiSlider : MonoBehaviour
+{
+
+    public static Slider m_Slider;//音量調整用スライダー
+
+    [SerializeField]
+    bool m_isInput;//キー入力で調整バーを動かせるようにするか
+    [SerializeField]
+
+    public static float GetMasterVolume()
+    {
+        return m_Slider.value;
+    }
+
+    void Awake()
+    {
+        m_Slider = GetComponent<Slider>();
+        m_Slider.value = GetMasterVolume();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
