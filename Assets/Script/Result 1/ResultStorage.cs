69fded92d72c93a0072064e36710fabd43989dd5
Ly4nke <s.kein.yosh@gmail.com>
2022-03-25 20:18:47 +0900

    データ送信を一回のみに制限。
    Flag処理よくわからないです。
    これでもいいかな。

--- Assets/Script/Result 1/ResultStorage.cs
+++ Assets/Script/Result 1/ResultStorage.cs
@@ -8,18 +8,26 @@ public class ResultStorage : MonoBehaviour
     public string Name;
     public int Score;
     public float Time;
+    public int flag;
     public InputField nameInput;
 
+        
     // Start is called before the first frame update
     void Start()
     {
+        flag = 0;
         Score = 3000 - LevelStorage.GetCash();
         Time = LevelStorage.GetTotalTime(0);
     }
     public void submitButton(){
+        if(flag == 0){
 		Name = nameInput.text;
 		Debug.Log (Name);
         Debug.Log (Score);
 		SaveScore.save(Name, Score, Time);
+        flag = 1;
+        }else{
+        Debug.Log("保存済みですよね？一回までだよ。");  
+        }
   }
 }
