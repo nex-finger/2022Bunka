7a45585d7f5d1cd209207db626b2dde7ef6045be
Ly4nke <s.kein.yosh@gmail.com>
2022-03-31 00:16:08 +0900

    ランキング表示の追加
    とりあえず簡単な実装。
    Result1の変更は名前を入力した際にランキングシーンに飛んだらフラグ処理いらないと思ってフラグ処理をとりあえずコメントアウト。
    シーンの追加と名前スコアタイムのトップ5を表示します。タイム昇順です。

---
+++ Assets/Script/Ranking/LeaderBoardManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+using UnityEngine.UI;
+
+public class LeaderBoardManager : MonoBehaviour
+{
+    private Text Ntext;
+    void Start () {
+
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("GameScore");
+
+         ///timeフィールドの降順でデータを取得
+                    query.OrderByAscending ("time");
+
+                    //検索件数を5件に設定
+                    query.Limit = 5;
+
+
+        query.FindAsync (async (List<NCMBObject> objList ,NCMBException e) => {
+                if (e != null) {
+                  UnityEngine.Debug.Log ("ランキング取得失敗");
+                } else {
+                    //検索成功時の処理
+                    UnityEngine.Debug.Log ("ランキング取得成功");
+                        //ログに表示
+                        foreach (NCMBObject obj in objList) {
+                            int    s = System.Convert.ToInt32(obj["score"]);
+					        string n = System.Convert.ToString(obj["name"]);
+					        float  t = System.Convert.ToSingle(obj["time"]);
+                            this.Ntext = GameObject.Find("NCMBText").GetComponent<Text>(); 
+                            Ntext.text +=  "名前" + n + "：スコア：" + s + "：タイム：" + t + "\n";
+                    }
+                }
+                                
+        });
+    }
+}
--- Assets/Script/Result 1/ResultStorage.cs
+++ Assets/Script/Result 1/ResultStorage.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResultStorage : MonoBehaviour
 {
     public string Name;
     public int Score;
     public float Time;
-    public int flag;
+    //public int flag;
     public InputField nameInput;
 
         
     // Start is called before the first frame update
     void Start()
     {
-        flag = 0;
+        //flag = 0;
         Score = 3000 - LevelStorage.GetCash();
         Time = LevelStorage.GetTotalTime(0);
     }
     public void submitButton(){
-        if(flag == 0){
+        /*if(flag == 0){
 		Name = nameInput.text;
 		Debug.Log (Name);
         Debug.Log (Score);
@@ -28,6 +29,11 @@ public class ResultStorage : MonoBehaviour
         flag = 1;
         }else{
         Debug.Log("保存済みですよね？一回までだよ。");  
-        }
+        }*/
+        Name = nameInput.text;
+		Debug.Log (Name);
+        Debug.Log (Score);
+		SaveScore.save(Name, Score, Time);
+        SceneManager.LoadScene("LeaderBoard");
   }
 }
