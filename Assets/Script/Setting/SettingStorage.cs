fd8666a810ff2f33f6d8f7223a36fc819c660bc2
nex-finger <2020Tibakou@gmail.com>
2022-04-01 17:41:10 +0900

    セーブしわすれてました
    はい

--- Assets/Script/Setting/SettingStorage.cs
+++ Assets/Script/Setting/SettingStorage.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingStorage : MonoBehaviour
+{
+    public static float Volume;
+    public static float Sensi_Rotate;
+    public static float Sensi_Zoom;
+
+    void Start()
+    {
+
+    }
+
+
+}
