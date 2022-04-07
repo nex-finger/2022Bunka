df2040b87f136691dd1272d5af1af3d83b6d3579
OrangeP1anet <kass12092@outlook.jp>
2022-04-07 23:45:03 +0900

    Add AnimationImage
    アニメーションに使う画像をmaterialに追加

---
+++ Assets/Script/SceneAnimation/Animation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class Animation : MonoBehaviour
+{  
+    void Start()
+    {
+        //transform.DOLocalMove(new Vector3(0, 288, 0), 1);
+    }
+}
