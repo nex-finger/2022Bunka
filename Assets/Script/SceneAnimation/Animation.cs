cf8453ca9016b9c68659d709312fdaadbaa9807b
OrangeP1anet <kass12092@outlook.jp>
2022-04-10 15:09:57 +0900

    Add transition
    タイトルからスタートのみトランジションの追加

--- Assets/Script/SceneAnimation/Animation.cs
+++ Assets/Script/SceneAnimation/Animation.cs
@@ -6,9 +6,37 @@ using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class Animation : MonoBehaviour
-{  
-    void Start()
+{
+    public GameObject Moveobj1;
+    public GameObject Moveobj2;
+    public GameObject Moveobj3;
+    public GameObject Moveobj4;
+    public GameObject CanvasAnim;
+
+    public void Anim1()
     {
-        //transform.DOLocalMove(new Vector3(0, 288, 0), 1);
+        DontDestroyOnLoad(CanvasAnim);
+        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                .OnComplete(SCArcadeLanding)
+                .SetEase(Ease.InOutCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                .SetEase(Ease.InOutCubic);
+    }
+
+    private void SCArcadeLanding()
+    {
+        SceneManager.LoadScene("ArcadeLanding");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
     }
 }
