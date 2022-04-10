7aaafc0f4949e1b0f356b92e7009ebc96112eaeb
OrangeP1anet <kass12092@outlook.jp>
2022-04-10 15:35:07 +0900

    Add transition
    タイトルから移動できるシーンすべてにトランジション追加

--- Assets/Script/SceneAnimation/Animation.cs
+++ Assets/Script/SceneAnimation/Animation.cs
@@ -11,25 +11,135 @@ public class Animation : MonoBehaviour
     public GameObject Moveobj2;
     public GameObject Moveobj3;
     public GameObject Moveobj4;
-    public GameObject CanvasAnim;
+    public GameObject Canvas;
 
-    public void Anim1()
+    //ボタンを押したときアニメーションする
+    public void SCArcadeLanding()
     {
-        DontDestroyOnLoad(CanvasAnim);
+        DontDestroyOnLoad(Canvas);
         Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
-                .OnComplete(SCArcadeLanding)
                 .SetEase(Ease.InOutCubic);
         Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
                 .SetEase(Ease.InOutCubic);
         Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
                 .SetEase(Ease.InOutCubic);
         Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
-                .SetEase(Ease.InOutCubic);
+                .SetEase(Ease.InOutCubic)
+                .OnComplete(LoadArcadeLanding);
     }
-
-    private void SCArcadeLanding()
+    //シーンを変更してその後にアニメ―ション
+    private void LoadArcadeLanding()
     {
         SceneManager.LoadScene("ArcadeLanding");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
+    }
+
+    public void SCLeaderBoard()
+    {
+        DontDestroyOnLoad(Canvas);
+        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                .SetEase(Ease.InOutCubic)
+                .OnComplete(LoadLeaderBoard);
+    }
+    private void LoadLeaderBoard()
+    {
+        SceneManager.LoadScene("LeaderBoard");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
+    }
+
+    public void SCGallery()
+    {
+        DontDestroyOnLoad(Canvas);
+        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                .SetEase(Ease.InOutCubic)
+                .OnComplete(LoadGallery);
+    }
+    private void LoadGallery()
+    {
+        SceneManager.LoadScene("Gallery");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
+    }
+
+    public void SCSetting()
+    {
+        DontDestroyOnLoad(Canvas);
+        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                .SetEase(Ease.InOutCubic)
+                .OnComplete(LoadSetting);
+    }
+    private void LoadSetting()
+    {
+        SceneManager.LoadScene("Setting");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
+    }
+
+    public void SCHowtoPlay()
+    {
+        Moveobj1 = GameObject.Find("expand01");
+        Moveobj2 = GameObject.Find("expand02");
+        Moveobj3 = GameObject.Find("expand03");
+        Moveobj4 = GameObject.Find("expand04");
+        Canvas = GameObject.Find("CanvasAnim");
+        DontDestroyOnLoad(Canvas);
+        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                .SetEase(Ease.InOutCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                .SetEase(Ease.InOutCubic)
+                .OnComplete(LoadHowtoPlay);
+    }
+    private void LoadHowtoPlay()
+    {
+        SceneManager.LoadScene("HowtoPlay");
         Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
             .SetEase(Ease.InCubic);
         Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
