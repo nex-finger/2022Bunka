0e7753bd764386b52ff4130ec648d4709fa22d3e
nex-finger <2020Tibakou@gmail.com>
2022-04-18 20:24:26 +0900

    はい
    むっず

--- Assets/Script/Audio/PlayBGM.cs
+++ Assets/Script/Audio/PlayBGM.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class PlayBGM : MonoBehaviour
 {
-    public AudioSource Music1;
-    public AudioSource Music2;
-    public AudioSource Music3;
-    public AudioSource Music4;
-    public AudioSource Voice1;
-    public AudioSource Voice2;
-    public AudioSource Voice3;
+    public AudioSource M1;
+    public AudioSource M2;
+    public AudioSource M3;
+    public AudioSource M4;
+    public AudioSource V1;
+    public AudioSource V2;
+    public AudioSource V3;
+
+    public static AudioSource Music1 = M1;
+    public static AudioSource Music2;
+    public static AudioSource Music3;
+    public static AudioSource Music4;
+    public static AudioSource Voice1;
+    public static AudioSource Voice2;
+    public static AudioSource Voice3;
+
+    //Music1 = Find("Beyond").AudioSource;
 
     public GameObject Script;
+
+    public static void PlayMusic1()
+    {
+        StopAudio();
+        Music1.Play();
+    }
+
+    public void PlayMusic2()
+    {
+        StopAudio();
+        Music2.Play();
+    }
+
+    public void PlayMusic3()
+    {
+        StopAudio();
+        Music3.Play();
+    }
+
+    public void PlayMusic4()
+    {
+        StopAudio();
+        Music4.Play();
+    }
+
+    void PlayVoice1()
+    {
+        StopAudio();
+        Voice1.Play();
+    }
+
+    public void PlayVoice2()
+    {
+        StopAudio();
+        Voice2.Play();
+    }
+
+    public void PlayVoice3()
+    {
+        StopAudio();
+        Voice3.Play();
+    }
+
+    public static void StopAudio()
+    {
+        Music1.Stop();
+        Music2.Stop();
+        Music3.Stop();
+        Music4.Stop();
+        Voice1.Stop();
+        Voice2.Stop();
+        Voice3.Stop();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,58 +89,7 @@ public class PlayBGM : MonoBehaviour
         DontDestroyOnLoad(Voice2);
         DontDestroyOnLoad(Voice3);
         //Audio1.Play();
-    }
 
-    void StopAudio()
-    {
-        Music1.Stop();
-        Music2.Stop();
-        Music3.Stop();
-        Music4.Stop();
-        Voice1.Stop();
-        Voice2.Stop();
-        Voice3.Stop();
-    }
-
-    void PlayMusic1()
-    {
-        StopAudio();
-        Music1.Play();
-    }
-
-    void PlayMusic2()
-    {
-        StopAudio();
-        Music2.Play();
-    }
-
-    void PlayMusic3()
-    {
-        StopAudio();
-        Music3.Play();
-    }
-
-    void PlayMusic4()
-    {
-        StopAudio();
-        Music4.Play();
-    }
-
-    void PlayVoice1()
-    {
-        StopAudio();
-        Voice1.Play();
-    }
-
-    void PlayVoice2()
-    {
-        StopAudio();
-        Voice2.Play();
-    }
-
-    void PlayVoice3()
-    {
-        StopAudio();
-        Voice3.Play();
+        PlayMusic1();
     }
 }
---
+++ Assets/Script/SceneChange/SCMusic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCMusic : MonoBehaviour
+{
+    void PlaySCBGM()
+    {
+        PlayBGM.PlayMusic1();
+    }
+}
