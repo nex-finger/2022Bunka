5ad5132f4347ae5a91566b75b1abe6d9110ca4bc
Ly4nke <s.kein.yosh@gmail.com>
2022-04-22 23:14:40 +0900

    BGMぶちこんでいくううううううううううううううううううう

---
+++ Assets/Script/BGM/CheckMainBGM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMainBGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObjectUtils.DestoryIfExist( "MainBGM" );
+    }
+}
