9538efb6d945646dc3ea6524b4b5792fec534a9c
nex-finger <2020Tibakou@gmail.com>
2022-04-24 01:43:23 +0900

    ごりごり
    オラ！

--- Assets/Script/FinishLanding.cs
+++ Assets/Script/FinishLanding.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,105 +8,8 @@ using DG.Tweening;
 public class FinishLanding : MonoBehaviour
 {
     public GameObject Rocket;
-    private Vector3 pos;
-    private Vector3 pos_p;
-    private Vector3 pos_pp;
-    private int flag;
-    private int count;
+    public float Angle;
 
-    public GameObject Moveobj1;
-    public GameObject Moveobj2;
-    public GameObject Moveobj3;
-    public GameObject Moveobj4;
-    public GameObject Canvas;
-
-    void Judge()
-    {
-        float vel1;
-        float vel2;
-
-        flag = 0;
-
-        vel1 = (pos_p.x - pos.x) * (pos_p.x - pos.x) + (pos_p.y - pos.y) * (pos_p.y - pos.y);
-        vel2 = (pos_pp.x - pos_p.x) * (pos_pp.x - pos_p.x) + (pos_pp.y - pos_p.y) * (pos_pp.y - pos_p.y);
-
-        if (vel1 == 0.0f && vel2 == 0.0f)
-        {
-            flag = 1;
-        }
-    }
-
-    public void SC()
-    {
-        DontDestroyOnLoad(Canvas);
-        Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
-                .SetEase(Ease.InOutCubic);
-        Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
-                .SetEase(Ease.InOutCubic);
-        Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
-                .SetEase(Ease.InOutCubic);
-        Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
-                .SetEase(Ease.InOutCubic)
-                .OnComplete(LS);
-    }
-    //シーンを変更してその後にアニメ―ション
-    private void LS()
-    {
-        SceneManager.LoadScene("Result1");
-        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
-            .SetEase(Ease.InCubic);
-        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
-                .SetEase(Ease.InCubic);
-        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
-                .SetEase(Ease.InCubic);
-        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
-                .SetEase(Ease.InCubic);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        pos = new Vector3(0.0f, 0.0f, 0.0f);
-        pos_p = new Vector3(1.0f, 1.0f, 1.0f);
-        pos_pp = new Vector3(2.0f, 2.0f, 2.0f);
-
-        flag = 0;
-        count = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        pos.x = Rocket.transform.position.x;
-        pos.y = Rocket.transform.position.y;
-
-        Judge();
-
-        if(flag == 1)
-        {
-            SC();
-        }
-
-        count++;
-        if(count == 5)
-        {
-            count = 0;
-            pos_p = new Vector3(pos.x, pos.y, 0.0f);
-            pos_pp = new Vector3(pos_p.x, pos_p.y, 0.0f);
-        }
-    }
-}*/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using DG.Tweening;
-
-public class FinishLanding : MonoBehaviour
-{
-    public GameObject Rocket;
     private int flag;
     private int count;
 
@@ -129,12 +32,31 @@ public class FinishLanding : MonoBehaviour
         if (velocity == new Vector2(0.0f, 0.0f))
         {
             flag = 1;
+
+            Angle = Rocket.transform.rotation.z;
+            if(Angle == 0.0)
+            {
+                flag = 2;
+            }
         }
     }
 
     private void LoadResult1()
     {
         SceneManager.LoadScene("Result1");
+        Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
+            .SetEase(Ease.InCubic);
+        Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj3.transform.DOLocalMove(new Vector3(-1024, -96, 0), 1)
+                .SetEase(Ease.InCubic);
+        Moveobj4.transform.DOLocalMove(new Vector3(1024, -288, 0), 1)
+                .SetEase(Ease.InCubic);
+    }
+
+    private void LoadResult2()
+    {
+        SceneManager.LoadScene("Result2");
         Moveobj1.transform.DOLocalMove(new Vector3(-1024, 288, 0), 1)
             .SetEase(Ease.InCubic);
         Moveobj2.transform.DOLocalMove(new Vector3(1024, 96, 0), 1)
@@ -183,6 +105,34 @@ public class FinishLanding : MonoBehaviour
                 Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
                         .SetEase(Ease.InOutCubic)
                         .OnComplete(LoadResult1);
+            }
+        }
+
+        if (flag == 2)
+        {
+            //ここにシーンチェンジをよろしくお願いします
+            //変移先はResult1
+
+            //シーンチェンジアニメーションに使うオブジェクト
+            Moveobj1 = GameObject.Find("expand01");
+            Moveobj2 = GameObject.Find("expand02");
+            Moveobj3 = GameObject.Find("expand03");
+            Moveobj4 = GameObject.Find("expand04");
+            Canvas = GameObject.Find("CanvasAnim");
+            DontDestroyOnLoad(Canvas);
+
+            if (Once == true)
+            {
+                Once = false;
+                Moveobj1.transform.DOLocalMove(new Vector3(0, 288, 0), 1)
+                        .SetEase(Ease.InOutCubic);
+                Moveobj2.transform.DOLocalMove(new Vector3(0, 96, 0), 1)
+                        .SetEase(Ease.InOutCubic);
+                Moveobj3.transform.DOLocalMove(new Vector3(0, -96, 0), 1)
+                        .SetEase(Ease.InOutCubic);
+                Moveobj4.transform.DOLocalMove(new Vector3(0, -288, 0), 1)
+                        .SetEase(Ease.InOutCubic)
+                        .OnComplete(LoadResult2);
             }
         }
     }
