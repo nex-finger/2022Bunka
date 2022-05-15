0091b0250b06f114c01c23cb1bc46065c71c7752
nex-finger <2020Tibakou@gmail.com>
2022-05-16 02:29:42 +0900

    owaowa
    kitui

--- Assets/Script/RocketComponent/StartRocket.cs
+++ Assets/Script/RocketComponent/StartRocket.cs
@@ -1,23 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Return : MonoBehaviour
+public class StartRocket : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DataStorage.InputLimit(1);
+        SettingStorage.InputSensi_Zoom(0, 0.1f);
+        SettingStorage.InputSensi_Rotate(0, 0.1f);
+
+        DataStorage.SaveData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            // タイトルシーンに戻る
-            Application.Quit();
-        }
+        
     }
 }
